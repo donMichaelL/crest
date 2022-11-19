7f0cccd6c6ae931822c5b274cb303c050bf8c1ba
Michael Loukeris <michael.loukeris@icloud.com>
2022-11-19 18:49:37 +0200

    Adding the option to run fd and cod in different container

--- settings.py
+++ settings.py
@@ -8,22 +8,27 @@ FUSION_GEO = os.getenv("FUSION_GEO", "http://localhost:8000/")
 
 NATIONAL_DB_BASE_URL = os.getenv("NATIONAL_DB_BASE_URL", "10.129.150.90:8282")
 
-
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 
+MINIMAL = int(os.getenv("MINIMAL", 0))
 
 KAFKA_TOPICS = (
     "TOP22_11_LPR_DONE",
     "TOP12_04_LPR_ALERT",
     "TOP22_02_OBJECT_RECO_DONE",
-    "TOP10_02_COD_ALERT",
-    "TOP22_05_FACE_RECO_DONE",
+
     "TOP21_01_COMMAND_CENTER_MISSION",
     "TOP12_05_VEHICLE_COUNT_EVENT",
     "TOP22_08_ACTIVITY_RECO_DONE"
 )
+
+if MINIMAL == 0:
+    KAFKA_TOPICS += (
+        "TOP10_02_COD_ALERT",
+        "TOP22_05_FACE_RECO_DONE",
+    )
 
 OFFSET_RESET = "latest"
 
